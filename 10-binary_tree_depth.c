6a6361cf81f6bf1ffd67d3b36add5d1f3e7a3610
aimad kacem <kacem.aimad@gmail.com>
2024-03-05 16:02:39 +0100

    10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+* binary_tree_depth - calculate depth of binary tree node
+* @tree: pointer to binary tree node
+* Return: depth of node
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth_node;
+
+	if (!tree->parent)
+		return (0);
+	depth_node = binary_tree_depth(tree->parent);
+	return (depth_node + 1);
+}
