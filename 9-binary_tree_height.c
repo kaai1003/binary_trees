6ea2ba6ea985152f6d130516edd6781d629e07f0
aimad kacem <kacem.aimad@gmail.com>
2024-03-05 15:40:59 +0100

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,9 +10,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	left_h = binary_tree_height(tree->left);
-	right_h = binary_tree_height(tree->right);
+	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	if (left_h > right_h)
-		return (left_h + 1);
-	return (right_h + 1);
+		return (left_h);
+	return (right_h);
 }
